8f157c458d6deb6dad5abcd9c21a888d2dc8a3ed
Benjamin Rokseth <bensinober@gmail.com>
2013-09-30 15:40:07 +0200

    start with empty page

--- deploy/salt/files/aktivehyller.sh
+++ deploy/salt/files/aktivehyller.sh
@@ -4,6 +4,6 @@ CHROMIUMPREFS=~/.config/chromium
 while true
 do
   #rm -rf \$CHROMIUMPREFS/Singleton*
-  $CHROMIUM --kiosk --disable-restore-session-state --child-clean-exit
+  $CHROMIUM --kiosk --disable-restore-session-state --child-clean-exit http://localhost:4567
   sleep 3s
 done
