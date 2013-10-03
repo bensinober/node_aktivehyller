cabb3b0d0f5a41270c097c3255a06c352ab1f9d9
Benjamin Rokseth <bensinober@gmail.com>
2013-10-03 16:21:04 +0200

    select manifestations

--- test/tests.js
+++ test/tests.js
@@ -11,14 +11,14 @@ describe('BOOK API', function() {
   describe('check book', function() {
     
     it('creates a book from tnr', function(done){
-      book.from_tnr(1, function() {
+      book.fromTnr(1, function() {
         expect(book.tnr).to.equal(1);
         done();
       });
     });
 
     it('creates a book uri', function(done){
-      book.from_tnr(1, function() {
+      book.fromTnr(1, function() {
         expect(book.uri).to.equal('http://data.deichman.no/resource/tnr_1');
         done();
       });
@@ -28,35 +28,35 @@ describe('BOOK API', function() {
       var config = {"endpoint": "http://data.deichman.no/sparql",
                      "base_uri": "http://data.lillehammer.no/resource/tnr_"};
       var book = new Book(config);
-      book.from_tnr(1, function() {
+      book.fromTnr(1, function() {
         expect(book.uri).to.equal('http://data.lillehammer.no/resource/tnr_1');
         done();
       });
     });    
  
     it('returns valid format for audiobook', function(done){
-      book.from_tnr(974232, function() {
+      book.fromTnr(974232, function() {
         expect(book.validFormat()).to.equal(true); 
         done();
       });      
     });
 
     it('returns valid even if only one of many formats is valid', function(done){
-      book.from_tnr(1447893, function() {
+      book.fromTnr(1447893, function() {
         expect(book.validFormat()).to.equal(true); 
         done();
       });
     });
 
     it('returns invalid format for music CD', function(done){
-      book.from_tnr(1031239, function() {
+      book.fromTnr(1031239, function() {
         expect(book.validFormat()).to.equal(false); 
         done();
       });
     });
 
     it('returns title when a book has an accepted format', function(done){
-      book.from_tnr(974232, function() {
+      book.fromTnr(974232, function() {
         expect(book.title).to.equal("Morgen i Jenin"); 
         done();
       });
@@ -66,7 +66,7 @@ describe('BOOK API', function() {
 
   describe('find book info', function() {
     it('returns authors of a book to an array', function(done){
-      book.from_tnr(974232, function() {
+      book.fromTnr(974232, function() {
         book.populate(function() {
           expect(book.authors[0]).to.have.keys('creatorName');
           done();
@@ -75,9 +75,37 @@ describe('BOOK API', function() {
     });
 
     it('sets responsible of book', function(done){
-      book.from_tnr(1429670, function() {
+      book.fromTnr(1429670, function() {
         book.populate(function() {
           expect(book.responsible).to.match(/Nils\ Gaup/);
+          done();
+        });
+      });
+    });
+
+    it('localReviews if empty if there are no local reviews', function(done){
+      book.fromTnr(382695, function() {
+        book.populate(function() {
+          expect(book.localReviews).to.be.empty();
+          done();
+        });
+      });
+    });
+
+    it('fetches local reviews', function(done){
+      book.fromTnr(1058670, function() {
+        book.populate(function() {
+          expect(book.localReviews.length).to.be.greaterThan(1);
+          expect(book.localReviews[0].reviewText.length).to.be.greaterThan(10);
+          done();
+        });
+      });
+    });
+
+    it('fetcher other works by author', function(done){
+      book.fromTnr(72680, function() {
+        book.populate(function() {
+          expect(book.sameAuthorBooks).to.not.be.empty();
           done();
         });
       });
