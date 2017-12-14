73cd33c2cb9ec573ca63deb0c325ca26cbf6e5fd
Benjamin Rokseth <bensinober@gmail.com>
2017-12-14 01:51:51 +0100

    Implement more from author view and similar works

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -13,3 +13,34 @@ export function doGetRequest(args) {
     return Promise.reject(`Failed fetching external resource: ${err}`)
   })
 }
+
+export function lookupBook(book) {
+  console.log(book)
+  // TODO: Rewrite into ractive
+  // show overlay
+  //$('button#retry-knapp').hide();
+  //$('#overlay').show();
+  //$('button#avbryt-knapp').html('Avbryt');
+  //$('div#vi-leter p').html("Henter info om \"" + title + '" <div id="loading"></div>' );
+  //$('#vi-leter').show();
+/*  doGetRequest(`/populate/${book.bookId}`)
+  .then(res => {
+    console.log(res);
+    this.set({book: response, reviewIndex: 0, reviewMode: true, moreFromAuthorMode: false, similarWorkMode: false});
+    var authors = [];
+    // comma, separated authors list
+    response.authors.forEach(function(a) {
+      authors.push(a.creatorName);
+    });
+    headerRactive.set({title: response.title, authors: authors.join(', ') });
+    // save to history
+    leftbarRactive.data.bookStable.push(response);
+    $('#overlay').hide();
+    $('#vi-leter').hide();
+  })
+  .catch(err => {
+    console.log(err)
+    $('div#vi-leter p').html('&#9760; Beklager, dette tok for lang tid!' );
+    $('button#retry-knapp').show();
+  })*/
+}
