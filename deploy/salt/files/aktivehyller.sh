fa71273c8d4cc1701cf624bcd1ddfcabca9c2817
Benjamin Rokseth <bensinober@gmail.com>
2017-12-16 23:40:16 +0100

    Remove slug

--- deploy/salt/files/aktivehyller.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CHROMIUM=/usr/bin/chromium-browser
-CHROMIUMPREFS=~/.config/chromium
-while true
-do
-  #rm -rf \$CHROMIUMPREFS/Singleton*
-  $CHROMIUM --kiosk --disable-restore-session-state --child-clean-exit http://localhost:4567
-  sleep 3s
-done
